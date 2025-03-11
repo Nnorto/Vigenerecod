a71a45945080175e890d4414899436fc43c15f4e
Nnorto <artem4icaz@gmail.com>
2025-03-11 22:38:37 +0300

    баги есть

--- vigenere.py
+++ vigenere.py
@@ -7,18 +7,19 @@ def vig(text, k, f, shift, language):
     key = cycl(text, k)
     ns = ''
     flag = f
+    startalf = shift
     if flag == 1:
         if language == 1:
             for i in range(len(text)):
-                ns += csrru(text[i], key[i], shift)
+                ns += csrru(text[i], key[i], startalf)
         else:
             for i in range(len(text)):
-                ns += csreng(text[i], key[i], shift)
+                ns += csreng(text[i], key[i], startalf)
     else:
         if language == 1:
             for i in range(len(text)):
-                ns += csrdru(text[i], key[i], shift)
+                ns += csrdru(text[i], key[i], startalf)
         else:
             for i in range(len(text)):
-                ns += csrdeng(text[i], key[i], shift)
+                ns += csrdeng(text[i], key[i], startalf)
     return ns
